706d50b70d3a67dd528d71f8831797d186e2179b
Austin Foxley <austinf@cetoncorp.com>
2012-02-22 23:13:11 -0800

    fix tags.sh
    Signed-off-by: Austin Foxley <austinf@cetoncorp.com>

--- tags.sh
+++ tags.sh
@@ -3,4 +3,4 @@
 find . -name '*.c' | awk '{print "\x22" $0 "\x22" }' > ./cscope.files
 find . -name '*.h' | awk '{print "\x22" $0 "\x22" }' >> ./cscope.files
 find /usr/include -name '*.h' | awk '{print "\x22" $0 "\x22" }' >> ./cscope.files
-cscope -b -q 2&>1 /dev/null
+cscope -b -q > /dev/null 2>&1
